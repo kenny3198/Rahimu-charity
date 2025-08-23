40fe5452640b7a34e7da4e71869d81b69192a93e
Abdulbaqi <abdulbaqhussain2018@gmail.com>
2025-08-22 19:27:45 -0700

    new feature

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,42 +1,3 @@
-// "use client"
-// import React from 'react'
-// import Bg from "../public/assets/rahim-hero.png"
-// import Image from 'next/image'
-// import { motion } from 'framer-motion'
-// const Hero = () => {
- 
-//   return (
-//      <div className="w-full h-screen relative overflow-hidden 
-//      flex items-center justify-center" >
-//       <Image 
-//          src={Bg} alt='image-hero' 
-//          layout='fill' objectFit='cover'
-          
-//          quality={100} priority={true} className='object-cover object-top'/>
-
-//          <motion.div 
-//           initial={{opacity:0,scale:0.5}}
-//                          whileInView={{opacity:1,scale:1}}
-//                          transition={{duration:1.5, delay:0.5}} 
-//          className='relative z-10 text-3xl  
-//           md:text-5xl font-extrabold text-gray-100 mb-2'>
-//               <div className='flex flex-col gap-3'>
-//                  <h1 className='md:text-4xl text-3xl text-center mb-3'>Rahimu Association for care and Charity</h1>
-//                 </div> 
-//                 <div>
-//                   <h2 className='text-2xl text-center'>uplifting the most
-//                      vulnerable members of society.</h2>
-//                      </div>            
-//          </motion.div>
-
-//     </div>
-//   )
-// }
-
-// export default Hero
-
-
-// hero.jsx
 "use client"
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
@@ -114,7 +75,7 @@ const Hero = () => {
         animate={{ opacity: 1, scale: 1 }}
         exit={{ opacity: 0 }}
         transition={{ duration: 1.5, delay: 0.5 }}
-        className="relative z-20 text-center text-gray-100 p-4"
+        className="relative z-20 text-center text-gray-100 p-4 m-10"
       >
         <h1 className="text-3xl md:text-4xl font-extrabold mb-3">
           {slide.title}
